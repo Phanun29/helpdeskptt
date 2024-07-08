6898a6b732b161c0be3782b24b3b4b99adc82245
Phanun29 <nenzigaming@gmail.com>
2024-07-08 16:49:05 +0700

    haha

--- index.php
+++ index.php
@@ -93,15 +93,11 @@
     <!-- /.login-box -->
 
     <!-- jQuery -->
-    <script src="../plugins/jquery/jquery.min.js"></script>
+    <script src="plugins/jquery/jquery.min.js"></script>
     <!-- Bootstrap 4 -->
-    <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- AdminLTE App -->
     <script src="../dist/js/adminlte.min.js"></script>
-
-    <!-- app -->
-    <script src="scripts/app.js"></script>
-    <script src="scripts/app.js"></script>
 </body>
 
 </html>
